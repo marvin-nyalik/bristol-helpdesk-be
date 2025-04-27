513fead6e217f92981792d98621d4f0813e9f82b
Janeirungu01 <jwirungu96@gmail.com>
2025-04-27 15:28:42 +0300

    add users endpoint

---
+++ app/controllers/users/users_controller.rb
@@ -0,0 +1,34 @@
+class Users::UsersController < ApplicationController
+    def index
+      render json: User.all
+    end
+  
+    def create
+      user = User.new(user_params)
+      if user.save
+        render json: user, status: :created
+      else
+        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  
+    def update
+      user = User.find(params[:id])
+      if user.update(user_params)
+        render json: user
+      else
+        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+
+    def getUsers
+      render json: User.all
+    end
+  
+    private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :username, :password, :user_type, branch_ids: [])
+    end
+end
+  
